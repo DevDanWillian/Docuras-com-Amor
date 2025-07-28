06cc46f082c3f8cd11eb933b0adcc04938a57b23
Daniel Willian Campos Silva <devdanielwillian@gmail.com>
2025-07-28 20:32:56 -0300

    "About Us" added!

--- src/app/components/textBody.tsx
+++ src/app/components/textBody.tsx
@@ -1,7 +1,12 @@
 const TextBody = () => {
   return(
     <>
-      <section className=""></section>
+      <section className="p-6 border-4 rounded border-verde-escuro text-lg">
+        <h2 className="text-3xl font-bold text-marrom-escuro mb-2">Sobre Nós</h2>
+        <p className="">A mais de 10 anos trazemos felicidade, começando como uma meta de renda extra, a fundadora Daniele, se esforçou incansavelmente para bater metas e melhorar a qualidade de produtos e disponibilidades dos mesmos.</p>
+        <p>Temos dos mais variados doces e salgados para o seu paladar!</p>
+        <a href="https://maps.app.goo.gl/2LEHaDzy9Sg1Z1Wh6" target="_blank" className="underline decoration-rosa-escuro">Estamos localizados na Rua Coelho Neto, 88 - Vila São Sebastião - Duque de Caxias.</a>
+      </section>
     </>
   )
 }
